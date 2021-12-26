a043c0154008be85cd0910bc2be489066be86522
Winn-Winn <scm.winwinhtay@gmail.com>
2021-12-26 16:50:04 +0630

    make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, tenetur rem? Debitis, accusamus et magnam vitae voluptates deserunt, eligendi voluptas, quas sapiente tempora architecto mollitia possimus. Adipisci provident velit aspernatur.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
