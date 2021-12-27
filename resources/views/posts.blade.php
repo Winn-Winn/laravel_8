27857cddae8892316a2fead11b38b99c995b2a47
Winn-Winn <scm.winwinhtay@gmail.com>
2021-12-27 19:43:53 +0630

    use the filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,27 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, tenetur rem? Debitis, accusamus et magnam vitae voluptates deserunt, eligendi voluptas, quas sapiente tempora architecto mollitia possimus. Adipisci provident velit aspernatur.
-        </p>
+        <?= $post; ?>
     </article>
-
-    <article>
-        <h1><a href="posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, tenetur rem? Debitis, accusamus et magnam vitae voluptates deserunt, eligendi voluptas, quas sapiente tempora architecto mollitia possimus. Adipisci provident velit aspernatur.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, tenetur rem? Debitis, accusamus et magnam vitae voluptates deserunt, eligendi voluptas, quas sapiente tempora architecto mollitia possimus. Adipisci provident velit aspernatur.
-        </p>
-    </article>
+    <?php endforeach; ?>
 </body>
