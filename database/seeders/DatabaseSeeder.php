86897c55c670b20250b5123394d27ffe793c58c9
Winn-Winn <scm.winwinhtay@gmail.com>
2021-12-29 16:38:17 +0630

    turbo boost with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,53 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
