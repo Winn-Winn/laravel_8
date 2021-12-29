71f1c201ff5df9af8219db0ffd8fec66e852cac3
Winn-Winn <scm.winwinhtay@gmail.com>
2021-12-29 15:44:03 +0630

    database seeding saves time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus! Dignissimos ipsam nisi ullam consequatur a sint, atque placeat ad molestias error, sit inventore laudantium asperiores ratione excepturi fugiat culpa.</p>'
+        ]);
     }
 }
