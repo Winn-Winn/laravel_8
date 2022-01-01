c717687ef57ab685ca19f7a85a957498b122e556
Winn-Winn <scm.winwinhtay@gmail.com>
2022-01-02 03:08:59 +0630

    write the markup for a post comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex space-x-4 bg-gray-200 p-5 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4>
+            <h3 class="font-bold">Awinn</h3>
+            <p class="text-xs">Posted 
+                <time>2 Days ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, totam! Ipsam asperiores nisi dignissimos debitis eaque obcaecati animi quam, odio aliquam rem id! Fugiat, magnam porro. Excepturi quae maxime voluptatibus!
+        </p>
+    </div>
+</article>
