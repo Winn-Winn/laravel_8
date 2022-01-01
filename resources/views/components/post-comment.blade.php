2a998d74baae526f16e3d9a090a6b36fc411d106
Winn-Winn <scm.winwinhtay@gmail.com>
2022-01-02 03:50:15 +0630

    make the comments section dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,18 @@
+@props(['comment'])
 <article class="flex space-x-4 bg-gray-200 p-5 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4>
-            <h3 class="font-bold">Awinn</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">Posted 
-                <time>2 Days ago</time>
+                <time>{{ $comment->created_at->diffForHumans() }}</time>
             </p>
         </header>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, totam! Ipsam asperiores nisi dignissimos debitis eaque obcaecati animi quam, odio aliquam rem id! Fugiat, magnam porro. Excepturi quae maxime voluptatibus!
+            {{ $comment->body }}
         </p>
     </div>
 </article>
