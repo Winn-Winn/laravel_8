f08887d7e1d1930a8041309f4470afbe63577ebe
Winn-Winn <scm.winwinhtay@gmail.com>
2022-01-02 02:27:26 +0630

    build the log in page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,8 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes))
+        {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcme Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+    }
+
     public function destroy()
     {
         auth()->logout();
